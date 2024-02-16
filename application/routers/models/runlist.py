cfd52756364bd734313a06434bfe60a459d93f13
urrutia <jurrutia@tacc.utexas.edu>
2024-02-16 15:46:17 -0600

    updating optional types to be nullable in pydantic v2

--- application/routers/models/runlist.py
+++ application/routers/models/runlist.py
@@ -9,18 +9,18 @@ __all__ = ["BiospecimenIds", "RunList", "RunListBase"]
 
 class BiospecimenIds(BaseModel):
     biospecimen_id: str
-    biospecimen_tracking_id: Optional[str]
+    biospecimen_tracking_id: Optional[str] = None
 
 
 class RunListBase(BaseModel):
     runlist_id: str
     name: str
-    description: Optional[str]
+    description: Optional[str] = None
     location_id: str
 
 
 class RunList(RunListBase):
-    status_name: Optional[str]
-    tracking_id: Optional[str]
+    status_name: Optional[str] = None
+    tracking_id: Optional[str] = None
     type: Optional[str]
-    location_display_name: Optional[str]
+    location_display_name: Optional[str] = None

e37eab510eaf4c3e7a1590887665e15a6e99912c
urrutia <jurrutia@tacc.utexas.edu>
2024-06-06 13:10:24 -0500

    updating view to catch v1 samples w/o a biosample or with only v3 biosamples

--- views/genomics_subjects_02.sql
+++ views/genomics_subjects_02.sql
@@ -1,8 +1,7 @@
 SELECT
     DISTINCT
-    sp.record_id as src_subject_id,
+    dem.record_id as src_subject_id,
     TO_CHAR(TO_DATE(bsdp.bscp_time_blood_draw, 'YYYY-MM-DD'), 'MM/DD/YYYY') as interview_date,
-    /* MONTHS_BETWEEN(TO_DATE(bsdp.bscp_time_blood_draw, 'YYYY-MM-DD'), TO_DATE(dem.brthdtc, 'YYYY-MM-DD'))  as interview_age, */
     ROUND( 
         EXTRACT(year FROM age(
             TO_DATE(bsdp.bscp_time_blood_draw, 'YYYY-MM-DD'),
@@ -17,18 +16,28 @@ SELECT
             TO_DATE(dem.brthdtc, 'YYYY-MM-DD')
             ))/30.437
      )::INTEGER as interview_age,
-    bsdp.sex,
-    bsdp.dem_race,
-    bsdp.ethnic,
-    surgery_type as phenotype,
+    dem.sex,
+    dem.dem_race,
+    dem.ethnic,
+    CASE 
+        WHEN CAST(dem.record_id AS INTEGER) BETWEEN 10000 AND 14999 THEN 'TKA'
+        WHEN CAST(dem.record_id AS INTEGER) BETWEEN 15000 AND 19999 THEN 'Thoracic'
+        WHEN CAST(dem.record_id AS INTEGER) BETWEEN 20000 AND 24999 THEN 'Thoracic'
+        WHEN CAST(dem.record_id AS INTEGER) BETWEEN 25000 AND 29999 THEN 'TKA'
+        ELSE NULL
+    END AS  phenotype,
     'No' as twins_study,
     'No' as sibling_study,
     'No' as family_study,
-    'Yes' as sample_taken
+    CASE 
+        WHEN bsdp.biospecimen_id IS NOT NULL
+        THEN 'Yes'
+        ELSE 'No' END AS sample_taken
      /* NDA_GUID as subjectkey, */
-FROM a2cps.biospecimens_details_private bsdp
-INNER JOIN a2cps.subjects_private sp
-    ON sp.subject_id=bsdp.subject_id
-INNER JOIN a2cps.rcap_patient_demographics_baseline_v03_demographics_i dem
+FROM  a2cps.rcap_patient_demographics_baseline_v03_demographics_i dem
+LEFT JOIN a2cps.subjects_private sp
     ON dem.record_id=sp.record_id
-WHERE bsdp.protocol_name = 'baseline_visit'
+LEFT JOIN a2cps.biospecimens_details_private bsdp
+    ON sp.subject_id=bsdp.subject_id  AND bsdp.protocol_name = 'baseline_visit'
+WHERE bsdp.protocol_name = 'baseline_visit' OR bsdp.protocol_name IS NULL
+
